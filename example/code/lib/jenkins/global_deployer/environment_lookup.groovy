9bf7f74d280f1e3e4996a0443c5c2ef77aa73864
Gabe E. Nydick <gnydick@imvu.com>
2021-03-10 13:05:20 -0800

    more proto updates

---
+++ example/code/lib/jenkins/global_deployer/environment_lookup.groovy
@@ -0,0 +1,30 @@
+import groovy.json.JsonSlurper
+
+
+try {
+    List<String> resultArray = new ArrayList<String>()
+    HttpURLConnection connection = new URL("http://qairon:5001/api/rest/v1/environment").openConnection()
+    connection.connect()
+    if (connection.responseCode == 200) {
+        jsonSlurper = new JsonSlurper()
+        // get the JSON response
+        inStream = connection.inputStream
+        environments = jsonSlurper.parse(inStream).objects
+        environments.objects
+        environments.each { environment  -> resultArray.add(environment.id)}
+        connection.disconnect()
+
+        return resultArray
+
+
+    } else {
+        print connection.responseCode + ": " + connection.inputStream.text
+
+    }
+
+
+
+} catch (e) {
+    print(e)
+}
+
