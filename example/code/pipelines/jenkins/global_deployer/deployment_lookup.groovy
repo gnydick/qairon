57e1fa32d96985d55ee3b1e78d889e8a7a36e829
Gabe E. Nydick <gnydick@imvu.com>
2021-03-15 10:55:01 -0700

    only need release ids for deployment lookup

--- example/code/pipelines/jenkins/global_deployer/deployment_lookup.groovy
+++ example/code/pipelines/jenkins/global_deployer/deployment_lookup.groovy
@@ -6,7 +6,7 @@ rel_ids = groovy.json.JsonOutput.toJson(RELEASE_ID.split(","))
 
 try {
     List<String> resultArray = new ArrayList<String>()
-    url = 'http://qairon:5001/api/rest/v1/deployment?q={"filters":[{"name":"service_id","op":"in","val":'+ svc_ids + '},{"name":"release_id","op":"in","val":'+ rel_ids + '}]}'
+    url = 'http://qairon:5001/api/rest/v1/deployment?q={"filters":[{"name":"release_id","op":"in","val":'+ rel_ids + '}]}'
 
     HttpURLConnection connection = new URL(url).openConnection()
     connection.connect()
