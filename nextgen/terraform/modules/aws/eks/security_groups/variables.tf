68962bc1be26533b1033f4a453fe906a563a75d0
mklipikov <mklipikov@imvu.com>
2021-04-26 18:59:23 +0300

    Updated variables.tf in EKS TF module

--- nextgen/terraform/modules/aws/eks/security_groups/variables.tf
+++ nextgen/terraform/modules/aws/eks/security_groups/variables.tf
@@ -1,10 +1,19 @@
 variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+  default     = ""
 }
 
 variable "environment" {
+  description = "Name of current environment"
+  type        = string
+  default     = ""
 }
 
 variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = ""
 }
 
 variable "config_name" {
--- nextgen/terraform/modules/aws/eks/variables.tf
+++ nextgen/terraform/modules/aws/eks/variables.tf
@@ -1,39 +1,118 @@
 variable "environment" {
+  description = "Name of current environment"
+  type        = string
+  default     = ""
 }
 
 variable "config_name" {
 }
 
 variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = ""
 }
 
-variable "vpc_cidr" {
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+  default     = ""
 }
 
 variable "azs" {
-  type = list(string)
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = []
 }
 
 variable "cluster_name" {
+  description = "Name of the EKS cluster. Also used as a prefix in names of related resources."
+  type        = string
 }
 
 variable "deployment_target" {
 }
 
 variable "eks_version" {
+  description = "Kubernetes version to use for the EKS cluster."
+  type        = string
 }
 
 variable "key_name" {
 }
 
-variable "vpc_id" {
+variable "cluster_endpoint_public_access" {
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled."
+  type        = bool
+  default     = true
 }
 
-variable "public_subnet_ids" {
+variable "cluster_endpoint_public_access_cidrs" {
+  description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint."
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "public_subnets_ids" {
+  description = "ID's of the Public subnets in the VPC"
   type = list(string)
 }
 
-variable "private_subnet_ids" {
+variable "private_subnets_ids" {
+  description = "ID's of the Private subnets in the VPC"
   type = list(string)
 }
 
+variable "cluster_service_ipv4_cidr" {
+  description = "service ipv4 cidr for the kubernetes cluster"
+  type        = string
+  default     = null
+}
+
+variable "cluster_endpoint_private_access" {
+  description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled."
+  type        = bool
+  default     = false
+}
+
+variable "cluster_create_security_group" {
+  description = "Whether to create a security group for the cluster or attach the cluster to `cluster_security_group_id`."
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources. Tags added to launch configuration or templates override these values for ASG Tags only."
+  type        = map(string)
+  default     = {}
+}
+
+variable "cluster_enabled_log_types" {
+  default     = []
+  description = "A list of the desired control plane logging to enable. For more information, see Amazon EKS Control Plane Logging documentation (https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html)"
+  type        = list(string)
+}
+
+variable "cluster_create_timeout" {
+  description = "Timeout value when creating the EKS cluster."
+  type        = string
+  default     = "35m"
+}
+
+variable "cluster_delete_timeout" {
+  description = "Timeout value when deleting the EKS cluster."
+  type        = string
+  default     = "20m"
+}
+
+variable "cluster_log_retention_in_days" {
+  default     = 90
+  description = "Number of days to retain log events. Default retention - 90 days."
+  type        = number
+}
+
+variable "cluster_iam_role_name" {
+  description = "IAM role name for the cluster."
+  type        = string
+  default     = ""
+}
