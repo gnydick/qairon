3b452cf9df3a2a07d9815c534d2aef82aa355026
mklipikov <mklipikov@imvu.com>
2021-04-26 14:09:34 +0300

    Update NAT gateways (In Progress)

--- nextgen/terraform/modules/aws/networking/main.tf
+++ nextgen/terraform/modules/aws/networking/main.tf
@@ -51,47 +51,45 @@ resource "aws_route" "public_internet_gateway" {
   }
 }
 
-
-
 ##########################################
 # NAT Gateways (Work in Progress)
 ##########################################
 
-#resource "aws_nat_gateway" "nat_gtw" {
-#  count = length(var.public_subnet_cidr)
-#
-#  allocation_id = element(
-#  local.nat_gateway_ips,
-#  var.single_nat_gateway ? 0 : count.index,
-#  )
-#  subnet_id = element(
-#  aws_subnet.public.*.id,
-#  var.single_nat_gateway ? 0 : count.index,
-#  )
-#
-#  tags = merge(
-#  {
-#    "Name" = format(
-#    "%s-%s",
-#    var.name,
-#    element(var.azs, var.single_nat_gateway ? 0 : count.index),
-#    )
-#  },
-#  var.tags,
-#  var.nat_gateway_tags,
-#  )
-#
-#  depends_on = [aws_internet_gateway.igw]
-#}
-#
-#resource "aws_route" "private_nat_gateway" {
-#  count = var.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0
-#
-#  route_table_id         = element(aws_route_table.private.*.id, count.index)
-#  destination_cidr_block = "0.0.0.0/0"
-#  nat_gateway_id         = element(aws_nat_gateway.this.*.id, count.index)
-#
-#  timeouts {
-#    create = "5m"
-#  }
-#}
+resource "aws_nat_gateway" "nat_gtw" {
+  count = length(var.public_subnet_cidr)
+
+  allocation_id = element(
+  local.nat_gateway_ips,
+  var.single_nat_gateway ? 0 : count.index,
+  )
+  subnet_id = element(
+  aws_subnet.public.*.id,
+  var.single_nat_gateway ? 0 : count.index,
+  )
+
+  tags = merge(
+  {
+    "Name" = format(
+    "%s-%s",
+    var.name,
+    element(var.azs, var.single_nat_gateway ? 0 : count.index),
+    )
+  },
+  var.tags,
+  var.nat_gateway_tags,
+  )
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count = var.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0
+
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.this.*.id, count.index)
+
+  timeouts {
+    create = "5m"
+  }
+}
