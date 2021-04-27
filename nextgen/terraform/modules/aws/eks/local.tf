6545ffba74460789bb8fdf7a7a7f5a915d286e96
mklipikov <mklipikov@imvu.com>
2021-04-27 13:38:44 +0300

    Added "cluster_iam_role_arn" to EKS local.tf

--- nextgen/terraform/modules/aws/eks/local.tf
+++ nextgen/terraform/modules/aws/eks/local.tf
@@ -1,5 +1,4 @@
 locals {
-
-  cluster_security_group_id = var.cluster_create_security_group ? join("", aws_security_group.cluster.*.id) : var.cluster_security_group_id
   cluster_iam_role_name             = join("", aws_iam_role.eks_service_role.*.name)
+  cluster_iam_role_arn              = join("", aws_iam_role.eks_service_role.*.arn)
 }
