c1a342698c46a33ebaa9f658497ec6b6d9907f05
mklipikov <mklipikov@imvu.com>
2021-04-28 17:23:16 +0300

    Added private subnet routing configuration to Networking TF module

--- nextgen/terraform/modules/aws/networking/main.tf
+++ nextgen/terraform/modules/aws/networking/main.tf
@@ -57,56 +57,83 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-###########################################
-## NAT Gateways
-###########################################
-#
-#resource "aws_eip" "nat" {
-#  count = var.enable_nat_gateway && false == var.reuse_nat_ips ? local.nat_gateway_count : 0
-#
-#  vpc = true
-#
-#  tags = merge(
-#  {
-#    "Name" = format(
-#    "%s-%s",
-#    var.name,
-#    element(var.azs, var.single_nat_gateway ? 0 : count.index),
-#    )
-#  },
-#  var.tags,
-#  var.nat_eip_tags,
-#  )
-#}
-#
-#resource "aws_nat_gateway" "nat_gtw" {
-#  count = length(var.public_subnets)
-#
-#  allocation_id = element(local.nat_gateway_ips)
-#  subnet_id = element(module.public.public_subnets_ids)
-#  tags = merge(
-#  {
-#    "Name" = format(
-#    "%s-%s",
-#    var.name,
-#    element(var.azs, count.index),
-#    )
-#  },
-#  var.tags,
-#  var.nat_gateway_tags,
-#  )
-#
-#  depends_on = [aws_internet_gateway.igw]
-#}
-#
-#resource "aws_route" "private_nat_gateway" {
-#  count = element(module.public.public_subnets_ids) >= 0
-#
-#  route_table_id         = element(aws_route_table.private.*.id, count.index)
-#  destination_cidr_block = "0.0.0.0/0"
-#  nat_gateway_id         = element(aws_nat_gateway.nat_gtw.*.id, count.index)
-#
-#  timeouts {
-#    create = "5m"
-#  }
-#}
+##########################################
+# NAT Gateways
+##########################################
+
+resource "aws_eip" "nat" {
+  count = length(var.azs) > 0 ? length(var.azs) : 0
+
+  vpc = true
+
+  tags = merge(
+  {
+    "Name" = format(
+    "%s-%s",
+    var.environment,
+    element(var.azs ? 0 : count.index),
+    )
+  },
+  var.tags,
+  )
+}
+
+resource "aws_nat_gateway" "nat_gtw" {
+  count = length(var.azs) > 0 ? length(var.azs) : 0
+
+  allocation_id = element(local.nat_gateway_ips)
+  subnet_id = element(module.public.public_subnets_ids)
+  tags = merge(
+  {
+    "Name" = format(
+    "%s-%s",
+    var.environment,
+    element(var.azs, count.index),
+    )
+  },
+  var.tags,
+  var.nat_gateway_tags,
+  )
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private_rt" {
+  count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  vpc_id = var.vpc_id
+
+  tags = merge(
+  {
+    "Name" = format(
+    "%s-${var.private_subnet_suffix}-%s",
+    var.environment,
+    element(var.azs, count.index),
+    )
+  },
+  var.tags,
+  var.private_route_table_tags,
+  )
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count = element(module.public.public_subnets_ids) >= 0
+
+  route_table_id         = element(aws_route_table.private_rt.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.nat_gtw.*.id, count.index)
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+
+  subnet_id = element(module.private.private_subnets_ids, count.index)
+  route_table_id = element(
+  aws_route_table.private_rt.*.id,
+  count.index,
+  )
+}
