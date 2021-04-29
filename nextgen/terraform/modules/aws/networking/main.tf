7c165043fc7c704fea4ffe593b9c3a7d3980d247
mklipikov <mklipikov@imvu.com>
2021-04-29 14:37:48 +0300

    Test updates of main.tf in Networking TF module

--- nextgen/terraform/modules/aws/networking/main.tf
+++ nextgen/terraform/modules/aws/networking/main.tf
@@ -1,25 +1,77 @@
-module "public" {
-  source      = "./public_subnet"
-  environment = var.environment
-  azs = var.azs
-  vpc_id = var.vpc_id
-  public_subnets = var.public_subnets
-  public_subnet_tags = var.public_subnet_tags
+#################
+# Public subnet
+#################
+resource "aws_subnet" "public" {
+
+  count = length(var.public_subnets)
+
+  vpc_id                          = var.vpc_id
+  cidr_block                      = var.public_subnets[count.index]
+  availability_zone               = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id            = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+  map_public_ip_on_launch         = var.map_public_ip_on_launch
+
+  tags = merge(
+  {
+    "Name" = format(
+    "%s-${var.public_subnet_suffix}-%s",
+    var.environment,
+    element(var.azs, count.index),
+    )
+  },
+  var.tags,
+  var.public_subnet_tags,
+  )
 }
 
-module "private" {
-  source = "./private_subnet"
-  environment = var.environment
-  azs = var.azs
-  vpc_id = var.vpc_id
-  private_subnets = var.private_subnets
-  private_subnet_tags = var.private_subnet_tags
+#################
+# Private subnet
+#################
+resource "aws_subnet" "private" {
+
+  count = length(var.private_subnets)
+
+  vpc_id                          = var.vpc_id
+  cidr_block                      = var.private_subnets[count.index]
+  availability_zone               = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id            = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+
+  tags = merge(
+  {
+    "Name" = format(
+    "%s-${var.private_subnet_suffix}-%s",
+    var.environment,
+    element(var.azs, count.index),
+    )
+  },
+  var.tags,
+  var.private_subnet_tags,
+  )
 }
+
+#module "public" {
+#  source      = "./public_subnet"
+#  environment = var.environment
+#  azs = var.azs
+#  vpc_id = var.vpc_id
+#  public_subnets = var.public_subnets
+#  public_subnet_tags = var.public_subnet_tags
+#}
+#
+#module "private" {
+#  source = "./private_subnet"
+#  environment = var.environment
+#  azs = var.azs
+#  vpc_id = var.vpc_id
+#  private_subnets = var.private_subnets
+#  private_subnet_tags = var.private_subnet_tags
+#}
 
 ###################
 # Internet Gateway
 ###################
 resource "aws_internet_gateway" "igw" {
+  count =  length(var.public_subnets) > 0 ? 1 : 0
   vpc_id = var.vpc_id
   tags = merge(
   {
@@ -33,6 +85,7 @@ resource "aws_internet_gateway" "igw" {
 # Public routes
 ################
 resource "aws_route_table" "public_rt" {
+  count = length(var.public_subnets) > 0 ? 1 : 0
   vpc_id = var.vpc_id
   tags = merge(
   {
@@ -43,9 +96,10 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route" "public_internet_gateway" {
-  route_table_id         = aws_route_table.public_rt.id
+  count = length(var.public_subnets) > 0 ? 1 : 0
+  route_table_id         = aws_route_table.public_rt[0].id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw[0].id
   timeouts {
     create = "5m"
   }
@@ -53,8 +107,8 @@ resource "aws_route" "public_internet_gateway" {
 
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
-  subnet_id      = element(module.public.public_subnets_ids, count.index)
-  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = element(aws_route_table.public_rt.*.id, count.index)
 }
 
 ##########################################
@@ -62,16 +116,15 @@ resource "aws_route_table_association" "public" {
 ##########################################
 
 resource "aws_eip" "nat" {
-  count = length(var.azs) > 0 ? length(var.azs) : 0
+  count = var.enable_nat_gateway ? local.nat_gateway_count : 0
 
   vpc = true
-
   tags = merge(
   {
     "Name" = format(
     "%s-%s",
     var.environment,
-    element(var.azs ? 0 : count.index),
+    element(var.azs, count.index),
     )
   },
   var.tags,
@@ -79,10 +132,10 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_nat_gateway" "nat_gtw" {
-  count = length(var.azs) > 0 ? length(var.azs) : 0
+  count = var.enable_nat_gateway ? local.nat_gateway_count : 0
 
-  allocation_id = element(local.nat_gateway_ips)
-  subnet_id = element(module.public.public_subnets_ids)
+  allocation_id = element(local.nat_gateway_ips, count.index)
+  subnet_id = element(aws_subnet.public.*.id, count.index)
   tags = merge(
   {
     "Name" = format(
@@ -103,10 +156,9 @@ resource "aws_nat_gateway" "nat_gtw" {
 ################
 
 resource "aws_route_table" "private_rt" {
-  count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
+  count = local.max_subnet_length > 0 ? local.nat_gateway_count : 0
 
   vpc_id = var.vpc_id
-
   tags = merge(
   {
     "Name" = format(
@@ -135,7 +187,7 @@ resource "aws_route" "private_nat_gateway" {
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
 
-  subnet_id = element(module.private.private_subnets_ids, count.index)
+  subnet_id = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(
   aws_route_table.private_rt.*.id,
   count.index,
