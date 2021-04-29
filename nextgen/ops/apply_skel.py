24dac358d6a9e450e81a3b2bddbd3982b5f959b4
Gabe E. Nydick <gnydick@imvu.com>
2021-04-28 22:01:28 -0700

    can update or generate

--- nextgen/ops/apply_skel.py
+++ nextgen/ops/apply_skel.py
@@ -2,6 +2,8 @@
 import argparse
 import os
 import shutil
+import requests
+import json
 
 # in the future, feed this from qairon to autogenerate
 
@@ -12,19 +14,37 @@ parser.add_argument('level', type=str, help='Options: provider, account, profile
 
 args = parser.parse_args()
 
-__levels__ = ['skel', 'provider', 'account', 'profile', 'region', 'target']
+__levels__ = ['skel', 'provider', 'account', 'profile', 'region']
 
 level = __levels__.index(args.level)
-print(level)
+
+profiles = {
+    '407733091588': 'build_user'
+}
 
 
 def process_dir(dir, iter):
-    source_dir = os.path.sep.join(__levels__[1:level])
+    source_dir = os.path.sep.join(__levels__[0:iter + 1])
     with os.scandir(dir) as scan_it:
-        # shutil.copy(os.path.sep.join(source_dir, '.envrc'), dir)
+        shutil.copy(os.path.sep.join([source_dir, '.envrc']), dir)
         print(dir)
         for entry in scan_it:
             if entry.is_dir():
                 process_dir(entry, iter + 1)
 
+
+# r = requests.get('http://localhost:5000/api/rest/v1/region')
+# data = json.loads(r.text)
+
+# regions = [[r['id'], r['pop']['id']] for r in data['objects']]
+# for reg, provider in regions:
+#     provider_deets = provider.split(':')
+#     reg_paths = reg.split(':')
+#     reg_paths.append(reg_paths[2])
+#     reg_paths[2] = 'build_user'
+#     suffix = os.path.sep.join(reg_paths)
+#     dr = os.path.sep.join(['generated', suffix])
+#     os.makedirs(dr, exist_ok=True)
+#     process_dir(os.path.sep.join(['generated', provider_deets[0]]), level)
 process_dir(args.dir, level)
+
