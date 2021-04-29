695f8608cba5e9c581f1c229757fd41b0a065700
mklipikov <mklipikov@imvu.com>
2021-04-29 14:39:13 +0300

    Removed sub-modules from the Networking TF module

--- nextgen/terraform/modules/aws/networking/private_subnet/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "private_subnets_ids" {
-  description = "List of IDs of private subnets"
-  value       = concat(aws_subnet.private.*.id, [""])[0]
-}
--- nextgen/terraform/modules/aws/networking/public_subnet/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "public_subnets_ids" {
-  description = "List of IDs of public subnets"
-  value       = concat(aws_subnet.public.*.id, [""])[0]
-}
