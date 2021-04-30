451d566d781b5478ab7e8ce714513ab8de885651
mklipikov <mklipikov@imvu.com>
2021-04-30 19:08:22 +0300

    Cleanup commented parts and unused parts from the "perf-max"

--- nextgen/terraform/provider/aws/accounts/EXAMPLE/us-west-2/perf-max/base/perf-1/main.tf
+++ nextgen/terraform/provider/aws/accounts/EXAMPLE/us-west-2/perf-max/base/perf-1/main.tf
@@ -17,38 +17,3 @@ module "cluster" {
   cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access
 }
 
-#//
-#//module "vpn" {
-#//  source = "openvpn"
-#//  key_name = "${var.key_name}"
-#//  environment = "${var.environment}"
-#//  extra_tags = {}
-#//  region = "${var.region}"
-#//  vpc_id = "${var.vpc_id}"
-#//  config_name = "${var.config_name}"
-#//  deployment_target = "${var.deployment_target}"
-#//  tier = "external"
-#//}
-
-#module "proxy_sg" {
-#  source            = "../../../../../../../modules/aws/eks-security-group"
-#  cluster_number    = var.cluster_number["perf-1"]
-#  config_name       = var.config_name
-#  deployment_target = var.deployment_target
-#  environment       = var.environment
-#  extra_tags = merge(
-#    var.extra_tags,
-#    {
-#      "Role" = "proxy"
-#    },
-#  )
-#  region = var.region
-#  vpc_id = var.vpc_id
-#}
-#
-#module "roles" {
-#  source      = "./roles"
-#  clustername = local.cluster_name
-#  environment = var.environment
-#}
-
