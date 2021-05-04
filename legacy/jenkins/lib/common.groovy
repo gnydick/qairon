e3f320636629dd21e9517037628c72463fa6ccb8
Gabe E. Nydick <gnydick@imvu.com>
2021-05-04 11:46:47 -0700

    updated common

--- legacy/jenkins/lib/common.groovy
+++ legacy/jenkins/lib/common.groovy
@@ -33,33 +33,68 @@ def notifySlack(String buildStatus = 'STARTED', String channel, String color = n
     slackSend(color: color, channel: channel, message: msg)
 }
 
-def node_with_secrets = { nodeLabel, container, function
-    ->
-    node(nodeLabel) {
-        try {
-            def secrets = [[$class: 'VaultSecret', path: 'cicd/aws_keys', secretValues: [
-                    [$class: 'VaultSecretValue', envVar: 'AWS_ACCESS_KEY_ID', vaultKey: 'AWS_ACCESS_KEY_ID'],
-                    [$class: 'VaultSecretValue', envVar: 'AWS_SECRET_ACCESS_KEY', vaultKey: 'AWS_SECRET_ACCESS_KEY']]],
-                           [$class: 'VaultSecret', path: 'cicd/bitbucket', secretValues: [
-                                   [$class: 'VaultSecretValue', envVar: 'BITBUCKET_USER', vaultKey: 'BITBUCKET_USER'],
-                                   [$class: 'VaultSecretValue', envVar: 'BITBUCKET_APP_PASSWORD', vaultKey: 'BITBUCKET_APP_PASSWORD']]]]
-            wrap([$class: 'VaultBuildWrapper', vaultSecrets: secrets]) {
-                properties([disableConcurrentBuilds()])
-                stage('Configure tools') {
-                    notifySlack()
-                    slackSend color: 'good', message: "`${env.JOB_NAME}` #${env.BUILD_NUMBER}\nRunning 'Configure tools' stage."
-                    container(name: container) {
-                        function(params)
+def node_with_secrets = { name, nodeLabel, container, function ->
+    timestamps {
+
+        podTemplate(name: name, label: nodeLabel, yaml: """
+kind: Pod
+metadata:
+  name: microservice-orchestration
+spec:
+  containers:
+  - name: microservice-orchestration
+    image: 407733091588.dkr.ecr.us-west-2.amazonaws.com/ego-cicd/microservice-orchestration:1.0.13
+    imagePullPolicy: Always
+    command:
+    - /bin/cat
+    tty: true
+"""
+        ) {
+            node(nodeLabel) {
+                def secrets = [[$class: 'VaultSecret', path: 'cicd/prod-1/aws_keys', secretValues: [
+                        [$class: 'VaultSecretValue', envVar: 'AWS_ACCESS_KEY_ID_PROD', vaultKey: 'AWS_ACCESS_KEY_ID'],
+                        [$class: 'VaultSecretValue', envVar: 'AWS_SECRET_ACCESS_KEY_PROD', vaultKey: 'AWS_SECRET_ACCESS_KEY']]],
+                               [$class: 'VaultSecret', path: 'cicd/aws_keys', secretValues: [
+                                       [$class: 'VaultSecretValue', envVar: 'AWS_ACCESS_KEY_ID_INT', vaultKey: 'AWS_ACCESS_KEY_ID'],
+                                       [$class: 'VaultSecretValue', envVar: 'AWS_SECRET_ACCESS_INT', vaultKey: 'AWS_SECRET_ACCESS_KEY']]],
+                               [$class: 'VaultSecret', path: 'cicd/bitbucket', secretValues: [
+                                       [$class: 'VaultSecretValue', envVar: 'BITBUCKET_USER', vaultKey: 'BITBUCKET_USER'],
+                                       [$class: 'VaultSecretValue', envVar: 'BITBUCKET_APP_PASSWORD', vaultKey: 'BITBUCKET_APP_PASSWORD']]],
+                               [$class: 'VaultSecret', path: 'cicd/artifactory', secretValues: [
+                                       [$class: 'VaultSecretValue', envVar: 'ART_TOOL_USER', vaultKey: 'ART_TOOL_USER'],
+                                       [$class: 'VaultSecretValue', envVar: 'ART_TOOL_PASSWORD', vaultKey: 'ART_TOOL_PASSWORD'],
+                                       [$class: 'VaultSecretValue', envVar: 'ART_BUILD_USER', vaultKey: 'ART_BUILD_USER'],
+                                       [$class: 'VaultSecretValue', envVar: 'ART_BUILD_PASSWORD', vaultKey: 'ART_BUILD_PASSWORD']]],
+                               [$class: 'VaultSecret', path: 'cicd/integration/user-manager', secretValues: [
+                                       [$class: 'VaultSecretValue', envVar: 'GOOGLE_APPLICATION_CREDENTIALS', vaultKey: 'GOOGLE_APPLICATION_CREDENTIALS'],
+                                       [$class: 'VaultSecretValue', envVar: 'GOOGLE_API_KEY', vaultKey: 'GOOGLE_API_KEY']]]]
+
+                wrap([$class: 'VaultBuildWrapper', vaultSecrets: secrets]) {
+                    properties([
+                            parameters([string(defaultValue: '', description: 'Tested microservice name', name: 'APP_NAME', trim: true),
+                                        string(defaultValue: '', description: 'Tested microservice version', name: 'APP_VERSION', trim: true),
+                                        choice(choices: ['int-1', 'int-2', 'prod-1'], description: 'Against which environment microservice will be tested?', name: 'CLUSTER_NAME')]),
+                            disableConcurrentBuilds()
+                    ])
+                    env.APP_NAME = "${params.APP_NAME}"
+                    env.APP_VERSION = "${params.APP_VERSION}"
+                    env.STACK_NAME = "env-${params.ENV_NAME}"
+                    env.CLUSTER_NAME = "${params.CLUSTER_NAME}"
+                    env.JOB_NAME = "${env.JOB_NAME}"
+                    env.BUILD_NUMBER = "${env.BUILD_NUMBER}"
+                    currentBuild.displayName = "#${env.BUILD_NUMBER} `${env.APP_NAME}-v${env.APP_VERSION}`"
+
+                    stage('Configure aws account and kubectl config') {
+
+                        container(container) {
+                            function()
+                        }
+
+
                     }
+
                 }
-            }
-        }
-        catch (e) {
-            currentBuild.result = 'FAILURE'
-            throw e
-        } finally {
-            notifySlack(currentBuild.result)
+            } // End timestamps
         }
     }
-
 }
