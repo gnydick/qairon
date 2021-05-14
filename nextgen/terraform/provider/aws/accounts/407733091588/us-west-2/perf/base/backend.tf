a1a3487c0018888a96a6f21b755fea2af34f959b
mklipikov <mklipikov@imvu.com>
2021-05-14 20:25:30 +0300

    Fix backend.tf value of the "key"

--- nextgen/terraform/provider/aws/accounts/407733091588/us-west-2/perf/base/backend.tf
+++ nextgen/terraform/provider/aws/accounts/407733091588/us-west-2/perf/base/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-backend-407733091588"
-    key            = "us-west-2/perf-max"
+    key            = "us-west-2/perf-1"
     region         = "us-west-2"
     dynamodb_table = "terraform-lock-state"
   }
