2a41cd27ad31de4467d4d48bdba69a9067890031
mklipikov <mklipikov@imvu.com>
2021-05-14 19:53:14 +0300

    Updated IAM Policy TF module

--- nextgen/terraform/modules/aws/iam-policy/main.tf
+++ nextgen/terraform/modules/aws/iam-policy/main.tf
@@ -1,10 +1,23 @@
+data "aws_iam_policy_document" "policy_document" {
+  statement {
+    sid       = var.iam_policy_document_sid
+    effect    = var.iam_policy_document_effect
+    actions   = var.iam_policy_document_actions
+    resources = var.iam_policy_document_resource_arn
+  }
+}
+
 resource "aws_iam_policy" "policy" {
   name        = var.name
   path        = var.path
   description = var.description
 
-  policy = var.policy
+  policy = data.aws_iam_policy_document.policy_document.json
 
   tags = var.tags
 }
 
+resource "aws_iam_role_policy_attachment" "attach_to_role" {
+  role       = var.iam_role_name
+  policy_arn = aws_iam_policy.policy.arn
+}
--- nextgen/terraform/modules/aws/iam-policy/variables.tf
+++ nextgen/terraform/modules/aws/iam-policy/variables.tf
@@ -1,29 +1,48 @@
 variable "name" {
   description = "The name of the policy"
   type        = string
-  default     = ""
+}
+
+variable "iam_role_name" {
+  description = "The name of the IAM role to which the policy should be applied"
+  type        = string
 }
 
 variable "path" {
   description = "The path of the policy in IAM"
   type        = string
-  default     = "/"
 }
 
 variable "description" {
   description = "The description of the policy"
   type        = string
-  default     = "IAM Policy"
 }
 
-variable "policy" {
-  description = "The path of the policy in IAM (tpl file)"
+variable "iam_policy_document_sid" {
+  description = "Sid for IAM policy statement"
   type        = string
-  default     = "./policy.tpl"
+}
+
+variable "iam_policy_document_effect" {
+  description = "IAM policy statement effect."
+  type        = string
+  validation {
+    condition     = var.iam_policy_document_effect == null ? true : contains(["Allow", "Deny"], var.iam_policy_document_effect)
+    error_message = "IAM policy statement effect must be either `null`, \"Allow\", or \"Deny\"."
+  }
+}
+
+variable "iam_policy_document_actions" {
+  description = "List of policy actions that will be added to the IAM Policy"
+  type        = list(string)
+}
+
+variable "iam_policy_document_resource_arn" {
+  description = "AWS Resource ARN"
+  type        = list(string)
 }
 
 variable "tags" {
   description = "A map of tags to add to all resources."
   type        = map(string)
-  default     = {}
 }
