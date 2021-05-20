57ac685a2082f11c70ab20a670a1f86bfa51046f
Gabe E. Nydick <gnydick@imvu.com>
2021-05-19 22:28:02 -0700

    added nodegroup modules

---
+++ nextgen/terraform/modules/aws/nodegroup-pair-sgs/main.tf
@@ -0,0 +1,40 @@
+
+resource "aws_security_group_rule" "a_b_in" {
+  description = "${var.nodegroup_a_name}-${var.nodegroup_b_name}-ingress"
+  source_security_group_id = "${var.nodegroup_a_id}"
+  from_port = 0
+  protocol = "all"
+  security_group_id = "${var.nodegroup_b_id}"
+  to_port = 0
+  type = "ingress"
+}
+
+resource "aws_security_group_rule" "b_a_in" {
+  description = "${var.nodegroup_b_name}-${var.nodegroup_a_name}-ingress"
+  source_security_group_id = "${var.nodegroup_b_id}"
+  from_port = 0
+  protocol = "all"
+  security_group_id = "${var.nodegroup_a_id}"
+  to_port = 0
+  type = "ingress"
+}
+
+resource "aws_security_group_rule" "a_b_out" {
+  description = "${var.nodegroup_a_name}-${var.nodegroup_b_name}-egress"
+  source_security_group_id = "${var.nodegroup_b_id}"
+  from_port = 0
+  protocol = "all"
+  security_group_id = "${var.nodegroup_a_id}"
+  to_port = 0
+  type = "egress"
+}
+
+resource "aws_security_group_rule" "b_a_out" {
+  description = "${var.nodegroup_b_name}-${var.nodegroup_a_name}-egress"
+  source_security_group_id = "${var.nodegroup_a_id}"
+  from_port = 0
+  protocol = "all"
+  security_group_id = "${var.nodegroup_b_id}"
+  to_port = 0
+  type = "egress"
+}
