3d82eee239e4a489a26370f78671bdf20f74985f
Gabe E. Nydick <gnydick@imvu.com>
2021-05-21 12:37:22 -0700

    stashing

---
+++ nextgen/terraform/provider/aws/accounts/126252960572/us-west-2/qairon/main.tf
@@ -0,0 +1,27 @@
+module "db" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+  version = "~> 3.0"
+
+  name           = "test-aurora-db-postgres96"
+  engine         = "aurora-postgresql"
+  engine_version = "11.9"
+  instance_type  = "db.r5.large"
+
+  vpc_id  = data.terraform_remote_state.vpc.outputs.vpc_ids["vpc1"]
+  subnets = ["subnet-12345678", "subnet-87654321"]
+
+  replica_count           = 1
+  allowed_security_groups = ["sg-12345678"]
+  allowed_cidr_blocks     = ["10.20.0.0/20"]
+
+  storage_encrypted   = true
+  apply_immediately   = true
+  monitoring_interval = 10
+
+  db_parameter_group_name         = "default"
+  db_cluster_parameter_group_name = "default"
+
+  enabled_cloudwatch_logs_exports = ["postgresql"]
+
+
+}
