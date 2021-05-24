e7108fb46dade88338de65ab1445f27188a09d15
Gabe E. Nydick <gnydick@imvu.com>
2021-05-24 08:48:01 -0700

    stashing

---
+++ nextgen/terraform/provider/aws/accounts/126252960572/us-west-2/perf/vpc/networking.tf
@@ -0,0 +1,10 @@
+module "networking" {
+  source               = "../../../../../../../modules/aws/modular_networking"
+  vpc_id               = module.vpc.vpc_id
+  public_subnet_cidrs  = var.public_subnets
+  private_subnet_cidrs = var.private_subnets
+  azs                  = var.azs
+  global_maps          = var.global_maps
+  global_strings       = var.global_strings
+}
+
