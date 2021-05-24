d0d04b3f498d8d2745bd5dc818da2e8aa72c6239
Gabe E. Nydick <gnydick@imvu.com>
2021-05-23 21:00:08 -0700

    fixed bad merges

--- nextgen/terraform/provider/aws/accounts/407733091588/us-west-2/infra/vpc/eks_target/networking.tf
+++ nextgen/terraform/provider/aws/accounts/407733091588/us-west-2/infra/vpc/eks_target/networking.tf
@@ -1,17 +1,11 @@
 module "networking" {
-<<<<<<< HEAD
-<<<<<<< HEAD:nextgen/terraform/provider/aws/accounts/407733091588/us-west-2/infra/vpc/eks_target/networking.tf
-   source = "../../../../../../../../../modules/aws/networking"
-=======
-   source = "../../../../../../../modules/aws/networking"
->>>>>>> eb2dda1ba89e7b3a1bfa4de8015ea445d7cb55b3:nextgen/terraform/provider/aws/accounts/296547766921/infra/vpc/eks_target/networking.tf
-=======
-   source = "../../../../../../../../../modules/aws/networking"
->>>>>>> eb2dda1ba89e7b3a1bfa4de8015ea445d7cb55b3
+   source = "../../../../../../../../modules/aws/networking"
    vpc_id = var.vpc_id
    public_subnets = var.public_subnets
    private_subnets = var.private_subnets
    azs = var.azs
    environment = var.environment
- }
+   global_maps = var.global_maps
+   global_strings = var.global_strings
+}
 
