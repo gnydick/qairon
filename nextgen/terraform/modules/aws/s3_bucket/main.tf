0d91907015523ddfddfe079ffa4ea7cca8a2f8cb
Gabe E. Nydick <gnydick@imvu.com>
2021-05-24 14:41:28 -0700

    stashing

--- nextgen/terraform/modules/aws/s3_bucket/main.tf
+++ nextgen/terraform/modules/aws/s3_bucket/main.tf
@@ -1,5 +1,6 @@
 locals {
   bucket = "${var.bucket_prefix}.s3-bucket"
+  logging_bucket = "${var.bucket_prefix}-logging.s3-bucket"
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
@@ -11,9 +12,29 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
   restrict_public_buckets = true
 
 }
+
+resource "aws_s3_bucket" "logging" {
+  bucket = local.logging_bucket
+  acl = "log-delivery-write"
+
+
+tags = merge(
+  var.tags,
+  {
+    "Name" = local.logging_bucket
+  }
+  )
+}
+
+
 resource "aws_s3_bucket" "bucket" {
   bucket = local.bucket
   acl    = var.s3_acl
+
+  logging {
+    target_bucket = aws_s3_bucket.logging.id
+    target_prefix = "log/"
+  }
 
 
   tags = merge(
--- nextgen/terraform/provider/aws/accounts/126252960572/us-west-2/qairon/jenkins-sa/main.tf
+++ nextgen/terraform/provider/aws/accounts/126252960572/us-west-2/qairon/jenkins-sa/main.tf
@@ -1,4 +1,5 @@
 resource "aws_iam_role" "jenks-sa" {
+  name = "Vpc0Infra0JenkinsSA"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -14,9 +15,42 @@ resource "aws_iam_role" "jenks-sa" {
           "${var.cluster_oidc_provider}:sub": "system:serviceaccount:default:jenkins"
         }
       }
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "sts.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
     }
   ]
 }
 EOF
 }
 
+resource "aws_iam_policy" "jenkins-sa-s3" {
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1621879441487",
+      "Action": [
+        "s3:DeleteObject",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::helm-repo-126252960572.s3-bucket"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins-sa-policy-attachment" {
+  policy_arn = aws_iam_policy.jenkins-sa-s3.arn
+  role = aws_iam_role.jenks-sa.name
+}
