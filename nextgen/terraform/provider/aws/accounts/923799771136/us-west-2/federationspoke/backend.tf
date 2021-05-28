f2be1cbb624e360f60d3bfeeaff4e1096f003b68
Gabe E. Nydick <gnydick@imvu.com>
2021-05-28 10:45:01 -0700

    setup xaccount assume roles

--- nextgen/terraform/provider/aws/accounts/923799771136/us-west-2/federationspoke/backend.tf
+++ nextgen/terraform/provider/aws/accounts/923799771136/us-west-2/federationspoke/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-backend-923799771136.s3-bucket"
-    key            = "us-west-2/prod"
+    key            = "us-west-2/federationspoke"
     region         = "us-west-2"
     dynamodb_table = "withme-engineering-prod-us-west-2-tfstate-default-923799771136.dynamodb-table"
   }
