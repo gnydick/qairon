096be6ebf462600bee8d721aef72d0eb3d9cc9fd
Gabe E. Nydick <gnydick@imvu.com>
2021-06-01 14:07:26 -0700

    first commit

--- nextgen/terraform/provider/aws/accounts/126252960572/us-west-2/jenkins_client/remote.tf
+++ nextgen/terraform/provider/aws/accounts/126252960572/us-west-2/jenkins_client/remote.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    key    = format("%s/%s", var.region, var.environment)
+    key    = format("%s/infra", var.region)
     bucket = format("terraform-state-backend-%s.s3-bucket", data.aws_caller_identity.core-lib.account_id)
     region = var.tfstate_region
   }
---
+++ nextgen/terraform/provider/aws/accounts/126252960572/us-west-2/jenkins_client/variables.tf
@@ -0,0 +1,1 @@
+variable "tfstate_region" {}
