c4f753e436f0c4e0bd9be2971941a0a687877955
Gabe E. Nydick <gnydick@imvu.com>
2021-07-31 15:34:46 -0700

    removed junk value

--- base.py
+++ base.py
@@ -4,7 +4,6 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.config['ENV'] = "FOO"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
