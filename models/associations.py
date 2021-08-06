a15e84b3d4c164ebf8466a24da1ab27ec444fc2b
Gabe E. Nydick <gnydick@imvu.com>
2021-08-05 23:08:28 -0700

    removed association table relationship for dep_rel

--- models/associations.py
+++ models/associations.py
@@ -12,11 +12,7 @@ subnets_to_fleets = Table('subnets_fleets', db.metadata,
                           Column('fleet_id', String, ForeignKey('fleet.id'))
                           )
 
-current_dep_release = Table('current_dep_release', db.metadata,
-                            Column('deployment_id', String, ForeignKey('deployment.id')),
-                            Column('release_id', String, ForeignKey('release.id')),
-                            UniqueConstraint('deployment_id')
-                            )
+
 
 svcs_to_repos = Table('services_repos', db.metadata,
                       Column('service_id', String, ForeignKey('service.id')),
