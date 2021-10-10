5d0a1cee2d2d0da76366185c3e070375eb5fef29
Gabe E. Nydick <gnydick@imvu.com>
2021-10-09 18:14:12 -0700

    updated qcli 0.16

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ importlib_backport_requires = ["importlib-metadata >= 0.23, < 5"]
 
 setup(
     name='qairon-qcli',
-    version='0.0.15',
+    version='0.0.16',
     url='https://github.com/gnydick/qairon',
     project_urls={},
     license='MIT',
