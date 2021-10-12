aa386d893a8357e3714b75255959aae891b1dc26
Gabe E. Nydick <gnydick@imvu.com>
2021-10-11 18:32:38 -0700

    tiny cleanup

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,3 @@
-
 from alembic import command
 from flask_migrate import Config
 from sqlalchemy.ext.compiler import compiles
@@ -7,16 +6,16 @@ from sqlalchemy.sql.ddl import DropTable
 from app import migrate
 from controllers import RestController, CLIArgs
 
+
 @compiles(DropTable, "postgresql")
 def _compile_drop_table(element, compiler, **kwargs):
     return compiler.visit_drop_table(element) + " CASCADE"
 
+
 def before_all(context):
     context.rest = RestController()
     context.args = CLIArgs(context.rest)
-    config = Config()
-
 
     migrate.db.drop_all()
     migrate.db.create_all()
-    print("hello")
+
