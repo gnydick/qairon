59b05d1e606c5cb1aabf7a234caf7694ae087e06
Gabe E. Nydick <gnydick@imvu.com>
2022-01-18 10:56:33 -0800

    updated how env vars are used

--- base.py
+++ base.py
@@ -2,11 +2,11 @@ import os
 
 from flask import Flask
 
+
 SQLALCHEMY_ENGINE_OPTIONS = {
     'pool_size': 32,
     'pool_recycle': 4,
     'pool_pre_ping': True,
-    'echo': True,
     'logging_name': 'PoolLog',
     'pool_use_lifo': True,
     'pool_timeout': 2
@@ -14,7 +14,8 @@ SQLALCHEMY_ENGINE_OPTIONS = {
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'SQLALCHEMY_TRACK_MODIFICATIONS' in os.environ
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = SQLALCHEMY_ENGINE_OPTIONS
+app.config['SQLALCHEMY_RECORD_QUERIES'] = 'SQLALCHEMY_RECORD_QUERIES' in os.environ
+app.config['SQLALCHEMY_ECHO'] = 'SQLALCHEMY_ECHO' in os.environ
 
-app.config['DEBUG'] = os.getenv('DEBUG', False)
