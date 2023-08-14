a51ef71fe5a53bb36f41b7e6d74dc8f7c92fcef4
Gabe E. Nydick <gnydick@imvu.com>
2023-08-14 10:34:25 -0700

    cleaned up plugins/init

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,7 +1,3 @@
-# import plugins.aws
-# import plugins.bake
-# import plugins.baker
-from sqlalchemy import event
-from sqlalchemy.ext.declarative import declarative_base
-
+import plugins.aws
+import plugins.bake
 import plugins.dependencies
