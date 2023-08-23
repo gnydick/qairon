888d009b4403fe11d1572817e0e0f432973927b3
Gabe E. Nydick <gnydick@imvu.com>
2023-08-23 14:53:55 -0700

    updated inventory generator to take the deployment_target_type on the command line

--- qcli_as_module/generate_dt_inventory_by_acount.py
+++ qcli_as_module/generate_dt_inventory_by_acount.py
@@ -1,3 +1,4 @@
+import argparse
 import collections
 import json
 import sys
@@ -11,38 +12,43 @@ sys.path.append(
     str(PROJECT_DIR)
 )
 eks_clusters = dict()
+def _main_():
+    parser = argparse.ArgumentParser(description='deployment_target inventory generator')
+    parser.add_argument('type', help='Deployment Target Type')
+    args = parser.parse_args()
+
+    def file(obj):
+        (_, _, account_id, _, _) = obj['partition_id'].split(':')
+
+        if account_id not in eks_clusters.keys():
+            eks_clusters[account_id] = list()
+        eks_clusters[account_id].append(obj['name'])
 
 
-def file(obj):
-    (_, _, account_id, _, _) = obj['partition_id'].split(':')
+    results = []
 
-    if account_id not in eks_clusters.keys():
-        eks_clusters[account_id] = list()
-    eks_clusters[account_id].append(obj['name'])
+    ioc = IterableOutputController(results)
 
+    cli = CLIController(ioc)
+    rest = RestController()
 
-results = []
+    ioc.handle(data=cli.query('deployment_target',
+                              query='[{"name": "deployment_target_type_id", "op":"eq", "val": "%s"}]' % args.type),
+               q=False)
+    dts = SerializableGenerator(results)
 
-ioc = IterableOutputController(results)
+    for data in dts:
+        obj = json.loads(data)
+        file(obj)
 
-cli = CLIController(ioc)
-rest = RestController()
+    od = dict(sorted(eks_clusters.items()))
 
-ioc.handle(data=cli.query('deployment_target',
-                          query='[{"name": "deployment_target_type_id", "op":"eq", "val": "emr"}]'),
-           q=False)
-dts = SerializableGenerator(results)
-
-for data in dts:
-    obj = json.loads(data)
-    file(obj)
-
-od = dict(sorted(eks_clusters.items()))
-
-for account, clusters in od.items():
-    print(account)
-    for cluster in clusters:
-        print("\t" + cluster)
+    for account, clusters in od.items():
+        print(account)
+        for cluster in clusters:
+            print("\t" + cluster)
 
 
 
+if __name__ == '__main__':
+    _main_()
