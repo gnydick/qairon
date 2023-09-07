6e8b39ffb07eb91bfa3f6d1fb6ae781a606350ce
Gabe E. Nydick <gnydick@imvu.com>
2023-09-07 05:39:15 -0700

    removed unused files from qcli package

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     extras_require={
         ':python_version == "3.9.2"': importlib_backport_requires
     },
-    packages=find_packages(exclude=("features", "features.*", "models", "views", "views.*")),
+    packages=find_packages(exclude=("features", "features.*", "models", "views", "views.*", "serializers" ,"serializers.*", "converters", "converters.*")),
     scripts=['qairon_qcli/qcli.py'],
     zip_safe=False,
     include_package_data=True,
