a2f6093d73f52f8bae36291ae8e0fdf37d1c54fb
Gabe E. Nydick <gnydick@nydick.net>
2026-01-16 18:37:21 -0800

    fix: update setup.py for modern pip/setuptools compatibility
    - Remove deprecated extras_require with environment marker syntax
    - Move importlib-metadata to install_requires with inline marker
    - Update json-stream and json-stream-rs-tokenizer to newer versions
    - Remove invalid [metadata] section from pyproject.toml

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ install_requires = ["argcomplete==2.0.0",
                     "itsdangerous==2.1.2",
                     "Jinja2==3.1.2",
                     "jmespath==1.0.1",
-                    "json-stream==2.1.0",
-                    "json-stream-rs-tokenizer==0.4.12",
+                    "json-stream>=2.3.0",
+                    "json-stream-rs-tokenizer>=0.5.0",
                     "MarkupSafe==2.1.1",
                     "python-dateutil==2.8.2",
                     "requests==2.28.1",
@@ -22,13 +22,13 @@ install_requires = ["argcomplete==2.0.0",
                     "six==1.16.0",
                     "urllib3==1.26.12",
                     "Werkzeug==2.2.2",
+                    "importlib-metadata >= 0.23, < 5; python_version < '3.10'",
                     ]
 tests_require = ["coverage", "flake8", "pexpect", "wheel", "behave"]
-importlib_backport_requires = ["importlib-metadata >= 0.23, < 5"]
 
 setup(
     name='qairon_qcli',
-    version='1.2.7',
+    version='1.2.8',
     url='https://github.com/gnydick/qairon',
     project_urls={},
     license='MIT',
@@ -39,9 +39,6 @@ setup(
     install_requires=install_requires,
     entry_points = {
         'console_scripts': ['qcli=qairon_qcli.qcli:_main_']
-    },
-    extras_require={
-        ':python_version == "3.9.2"': importlib_backport_requires
     },
     packages=find_packages(exclude=("features", "features.*", "models", "views", "views.*", "serializers" ,"serializers.*", "converters", "converters.*")),
     scripts=['qairon_qcli/qcli.py'],
